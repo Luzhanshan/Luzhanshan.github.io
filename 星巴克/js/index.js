1b6e75d191647fd97ab72564fee4c6219babd2a9
小明 <1282145151@qq.com>
2020-07-02 21:00:45 +0800

    更新小屏幕功能

--- 星巴克/js/index.js
+++ 星巴克/js/index.js
@@ -1,5 +1,9 @@
 $('.right_5_prev').click(function(){
-    $('.right_5_banner').animate({marginLeft : '140px'},500);
+    if(window.document.body.clientWidth<768){
+        $('.right_5_banner').animate({marginLeft : '0px'},500);
+    }else{
+        $('.right_5_banner').animate({marginLeft : '140px'},500);
+    }
     // $('.right_5_prev').css('display','none')
     // $('.right_5_next').css('display','block')
     $('.right_5_prev').fadeOut(500)
@@ -14,11 +18,10 @@ $('.right_5_next').click(function(){
     $('.right_5_next').fadeOut(500)
 })
 
-window.addEventListener('resize',function(){
-    var windowWidth = document.body.clientWidth;
-    if(windowWidth <1200){
-        $('.right_1').css('margin-top','180px');
-    }else{
-        $('.right_1').css('margin-top','0')
-    }
-})
+$('.green').css('display', 'none')
+	$('.green').eq(0).css('display', 'block')
+	$('.right_7_bottom ul li').click(function () {
+		$(this).find('a').addClass('active').end().siblings().find('a').removeClass('active');
+		$('.right_7_bottom ul li').find('.green').css('display', 'none')
+		$(this).find('.green').css('display', 'block')
+	})
